a66b578fc34ea9178265f180b30ee0bd797cd7f3
Matheus Augusto Paschuinio <matheus.paschuinio@fatec.sp.gov.br>
2026-03-02 17:11:58 -0300

    feat: criacao das migrations do sistema

---
+++ database/migrations/2026_03_02_193955_create_avisos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('avisos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('descricao');
+            $table->date('data_publicacao');
+            $table->foreignId('turma_id')->constrained('turmas');
+            $table->foreignId('autor_id')->constrained('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('avisos');
+    }
+};
