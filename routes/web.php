afee305f09a442479832c63f93b57d8dc276c806
Matheus Paschuinio <paschuiniomatheus@gmail.com>
2026-03-05 11:03:23 -0300

    feat: tela de inicio e sobre nós

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('inicio');
+Route::get('/sobre', function() {
+    return view('sobre');
+})->name('sobre');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
