612948a917838ab916e1846248c028f40e78bc87
Matheus Paschuinio <paschuiniomatheus@gmail.com>
2026-03-05 12:31:39 -0300

    fix: corrige erro de sintaxe na regra de validacao do telefone

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,7 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'telefone' => ['required', 'string', 'max:14', 'unique'.User::class],
+            'telefone' => ['required', 'string', 'max:14', 'unique:'.User::class],
             'tipo_usuario' => ['required', 'string', 'in:catequista,catequizando']
         ]);
 
